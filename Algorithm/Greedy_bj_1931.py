fadeb9fe38f2aafa028d898e4a6000de3cfeac3c
z2soo <moonjstars96@gmail.com>
2020-02-11 15:46:13 +0900

    파일정리

--- Algorithm/Greedy_bj_1931.py
+++ Algorithm/Greedy_bj_1931.py
@@ -13,5 +13,16 @@ def cnt_meeting(time_list):
 
 print(cnt_meeting(time_list))
 
+######################################
 
+    import sys
 
+    N = int(sys.stdin.readline())
+    meeting = sorted([list(map(int, sys.stdin.readline().split())) for i in range(N)], key=lambda x: (x[1], x[0]))
+
+    end = answer = 0
+    for m in meeting:
+        if end <= m[0]:
+            end = m[1]
+            answer += 1
+    print(answer)
