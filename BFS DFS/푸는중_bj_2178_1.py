ff358563d86b0fa69cad07bfbab863d3e9ea7875
unknown <moonjstars96@gmail.com>
2020-08-08 16:12:40 +0900

    algorithm exercise

--- BFS DFS/푸는중_bj_2178_1.py
+++ BFS DFS/푸는중_bj_2178_1.py
@@ -18,3 +18,58 @@
 
 출력
 첫째 줄에 지나야 하는 최소의 칸 수를 출력
+'''
+'''
+(1, 1) : 시작 
+(N, M) : 목표
+myList : 미로
+'''
+
+# 입력받기
+N, M = map(int, input().split())
+myList = []
+for _ in range(N):
+    m = list(input())
+    m = [int(_) for _ in m]
+    myList.append(m)
+print(myList)
+
+# 필요한 내용
+# (1,1)부터 시작
+# 인근이 0인지 1인지 확인 및 저장? 카운트?
+# (N,M)이 되면 멈춤
+
+
+# 방문 유무 표시 
+visited = [[0 for _ in range(M)] for i in range(N)]
+result = [[0 for _ in range(M)] for i in range(N)]
+print(visited)
+
+cnt = 0
+que = []
+
+directions = (0,1),(0,1),(-1,0),(0,-1)
+
+
+def miro():
+    global N, M, myList
+    que.append((1,1))
+    cnt = 0
+    while que:
+        x, y = que.pop(0)
+        print(x,y) 
+        for dx, dy in directions:
+            x = x + dx
+            y = y + dy
+            if x < 0 or y < 0:
+                #범위를 벗어난 경우 처리
+                print(x,y)
+            if visited[x][y] == 0:
+                visited[x][y] = 1  #방문처리
+                if myList[x][y] = 0:
+                #못지나가는 경우
+                else:
+
+                cnt += 1
+                result[x][y] = cnt 
+
